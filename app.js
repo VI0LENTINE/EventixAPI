bab7107b7b661ddcf331f25041ac37157b5de07c
VI0LENTINE <W0516070@campus.nscc.ca>
2025-11-15 14:18:12 -0400

    Trello Board Task 1 and 2 done. Imports and intro content finished.

---
+++ app.js
@@ -0,0 +1,34 @@
+// -- IMPORTS --
+import http from 'http';
+import express from 'express';
+import sql from 'mssql';
+
+
+// -- HTTP --
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html'});
+  res.write('hello world');
+  res.end();
+}).listen(3000);
+
+
+// -- EXPRESS -- 
+const port = process.env.PORT || 3000;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.send('Hello Express!');
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+
+// -- MSSQL --
+async() => {
+    await sql.connect(/* connection string goes here*/);
+    const result = await sql.query`select * from Table`;
+    const records = result.recordset;
+}
